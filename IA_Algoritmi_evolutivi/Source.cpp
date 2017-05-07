306b53457700514f387e52b506ff27f1b627428c
ureniucbogdan <ureniucbogdan@gmail.com>
2017-05-08 00:08:17 +0300

    test

--- IA_Algoritmi_evolutivi/Source.cpp
+++ IA_Algoritmi_evolutivi/Source.cpp
@@ -1,11 +1,20 @@
 #include <iostream>
+#include <fstream>
 
 class evolutiv {
-
+	int nr_thread;
+	int nr_procese;
 public:
 	evolutiv() {
-		
+		citire_date();
 	}
 
+
+	void citire_date() {
+		std::ifstream f("inp.txt");
+		f >> nr_thread;
+		f >> nr_procese;
+		f.close();
+	}
 
 };
